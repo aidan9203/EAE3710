cf5cb1322c376f873c72e948cd2d54a5a4a9651d
aidan9203 <aidan9203@outlook.com>
2020-02-14 02:15:40 -0700

    Smoothed out motion for gravity transitions between most surfaces

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -12,6 +12,8 @@ public class PlayerMovement : MonoBehaviour
 	Rigidbody rb;
 
 	float rotation = 0;
+	float rotation_d = 0;
+	float rotation_offset = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -38,12 +40,13 @@ public class PlayerMovement : MonoBehaviour
 
 		rotation += 180.0f * input_rotate * Time.deltaTime;
 
-		float move_horizontal = input_horizontal * Mathf.Cos(Mathf.Deg2Rad * rotation) + input_vertical * Mathf.Sin(Mathf.Deg2Rad * rotation);
-		float move_vertical = input_vertical * Mathf.Cos(Mathf.Deg2Rad * rotation) - input_horizontal * Mathf.Sin(Mathf.Deg2Rad * rotation);
+		float move_horizontal = input_horizontal * Mathf.Cos(Mathf.Deg2Rad * (rotation + rotation_offset)) + input_vertical * Mathf.Sin(Mathf.Deg2Rad * (rotation + rotation_offset));
+		float move_vertical = input_vertical * Mathf.Cos(Mathf.Deg2Rad * (rotation + rotation_offset)) - input_horizontal * Mathf.Sin(Mathf.Deg2Rad * (rotation + rotation_offset));
 
 		//Orient the player with gravity
 		tf.up = Vector3.Lerp(tf.up, -gravity, 0.05f);
-		tf.RotateAround(tf.position, tf.up, rotation);
+		rotation_d = Vector2.Lerp(new Vector2(rotation_d, 0), new Vector2(rotation_offset, 0), 0.05f).x;
+		tf.RotateAround(tf.position, tf.up, rotation + rotation_d);
 
 		//Apply motion relative to gravity
 		float vel_x = speed * (-Mathf.Abs(gravity.y) * move_horizontal - Mathf.Abs(gravity.z) * move_horizontal) + (Mathf.Abs(rb.velocity.x) + 9.81f * Time.deltaTime) * gravity.x;
@@ -60,20 +63,20 @@ public class PlayerMovement : MonoBehaviour
 	public void ChangeGravity(Vector3 g)
 	{
 		g.Normalize();
-		if (gravity.x > 0 && g.z > 0) { rotation -= 90; }
-		if (gravity.z > 0 && g.x > 0) { rotation += 90; }
+		if (gravity.x > 0 && g.z > 0) { rotation_offset -= 90; }
+		if (gravity.z > 0 && g.x > 0) { rotation_offset += 90; }
 
-		if (gravity.x > 0 && g.z < 0) { rotation += 90; }
-		if (gravity.z > 0 && g.x < 0) { rotation -= 90; }
+		if (gravity.x > 0 && g.z < 0) { rotation_offset += 90; }
+		if (gravity.z > 0 && g.x < 0) { rotation_offset -= 90; }
 
-		if (gravity.x < 0 && g.z > 0) { rotation += 90; }
-		if (gravity.z < 0 && g.x > 0) { rotation -= 90; }
+		if (gravity.x < 0 && g.z > 0) { rotation_offset += 90; }
+		if (gravity.z < 0 && g.x > 0) { rotation_offset -= 90; }
 
-		if (gravity.x < 0 && g.z < 0) { rotation -= 90; }
-		if (gravity.z < 0 && g.x < 0) { rotation += 90; }
+		if (gravity.x < 0 && g.z < 0) { rotation_offset -= 90; }
+		if (gravity.z < 0 && g.x < 0) { rotation_offset += 90; }
 
-		if (gravity.x != 0 && g.y > 0) { rotation += 180; }
-		if (gravity.y > 0 && g.x != 0) { rotation += 180; }
+		if (gravity.x != 0 && g.y > 0) { rotation_offset += 180; }
+		if (gravity.y > 0 && g.x != 0) { rotation_offset -= 180; }
 		gravity = g;
 	}
 }
