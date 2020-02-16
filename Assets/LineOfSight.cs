61f260bda208b0ea665b12bcf1b74700583153b2
Thomas Roberts <robertsorange@gmail.com>
2020-02-16 16:23:11 -0700

    Initial enemy line of sight tests

---
+++ Assets/LineOfSight.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineOfSight : MonoBehaviour
+{
+    public Transform playerTransform;
+    public float viewAngle = 20.0f;
+    public float viewDistance = 10.0f;
+    [Range(0, 1)]
+    public float trackingSpeed = 0.1f;
+
+
+    private int tempI = 0; // Used for logging if player is visible
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Calculate direction to player
+        Vector3 direction = playerTransform.position - transform.position;
+        
+        float angle = Vector3.Angle(direction, transform.forward);
+        if(angle < viewAngle && Vector3.Distance(transform.position, playerTransform.position) < viewDistance) {
+            Debug.Log("Player visible " + tempI);
+            tempI++;
+
+            // Rotate towards player
+            Quaternion newAngle = Quaternion.LookRotation(playerTransform.position - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, newAngle, trackingSpeed);
+        }
+    }
+}
