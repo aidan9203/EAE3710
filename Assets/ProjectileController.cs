f8bf66fb148c52a7c221d1dad9f09430d8b12ed2
Thomas Roberts <u0775084@ad.utah.edu>
2020-02-20 11:28:55 -0700

    Added key

--- Assets/ProjectileController.cs
+++ Assets/ProjectileController.cs
@@ -14,7 +14,7 @@ public class ProjectileController : MonoBehaviour
 
     void OnCollisionEnter(Collision collision) {
         if(collision.gameObject.CompareTag("PlayerModel")) {
-            collision.gameObject.SetActive(false);
+            collision.gameObject.GetComponent<PlayerMovement>().alive = false;
         }
         // Collided with a wall or something
         Destroy(gameObject);
