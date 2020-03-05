ed1ce123569ef3380c8cd58bd8ff4e0b738a2c8b
Thomas Roberts <u0775084@ad.utah.edu>
2020-03-05 10:39:51 -0700

    Added hit event for projectile

--- Assets/Enemies/ProjectileController.cs
+++ Assets/Enemies/ProjectileController.cs
@@ -1,12 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Events;
 
 public class ProjectileController : MonoBehaviour
 {
+    public UnityEvent playerHitEvent;
+
     void OnCollisionEnter(Collision collision) {
         if(collision.gameObject.CompareTag("Player")) {
             collision.gameObject.GetComponent<PlayerMovement>().alive = false;
+            playerHitEvent?.Invoke();
         }
         // Collided with a wall or something
         Destroy(gameObject);
