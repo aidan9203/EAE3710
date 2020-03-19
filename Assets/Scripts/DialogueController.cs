ae3f9022c4d789cec13d9a215aee77d1a46efaa4
Thomas Roberts <robertsorange@gmail.com>
2020-03-18 19:42:57 -0600

    Basic sentence printing to console

---
+++ Assets/Scripts/DialogueController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DialogueController : MonoBehaviour
+{
+    [TextArea(3, 4)]
+    public string[] sentences;
+    public bool listens = false;
+
+    private void OnTriggerEnter(Collider other) {
+        // Display our message when dialogue is triggered
+        if(other.CompareTag("Player")) {
+            Debug.Log("Player entered");
+            foreach (string sentence in sentences) {
+                Debug.Log(sentence);
+            }
+        }
+    }
+}
