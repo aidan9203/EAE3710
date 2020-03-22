37b4768b9ba38f7df5666c55ccde74bde95ab909
Thomas Roberts <robertsorange@gmail.com>
2020-03-21 22:12:32 -0600

    Added textmeshpro

--- Assets/Scripts/DatalogController.cs
+++ Assets/Scripts/DatalogController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 using UnityEngine.Rendering.PostProcessing;
+using TMPro;
 
 public class DatalogController : MonoBehaviour
 {
@@ -11,13 +12,13 @@ public class DatalogController : MonoBehaviour
     public string[] sentences;
     // Testing different styles of text management
     public bool listens = false;
-    public Text messageTextUI;
+    public TextMeshProUGUI messageTextUI;
     public PostProcessVolume ppVolume;
     
     private float aperture = 0.05f;
     private float focalLength = 300f;
     private float blurTime = 2f;
-    private bool triggered = false;
+    private bool triggered = false; // Prevent duplicate text boxes
 
     PostProcessVolume blurVolume;
 
@@ -34,42 +35,21 @@ public class DatalogController : MonoBehaviour
         blurVolume.weight = 0;
     }
 
-    private void OnTriggerEnter(Collider other) {
-        if(!listens) {
-            // Display our message when dialogue is triggered
-            if (other.CompareTag("Player") && !triggered) {
-                triggered = true;
-                datalogTriggered?.Invoke(); // Potentially could be using time.timescale(0). Discuss with group about best strategy to pause game
-
-                DisplayMessage();
-            }
-        }
-    }
-
     private void OnTriggerStay(Collider other) {
         if(Input.GetKeyDown(KeyCode.E) && other.CompareTag("Player")) {
             // Preventing multiple activations
             if(!triggered) {
                 triggered = true;
-                // Display the text
                 DisplayMessage();
             }
         }
     }
 
     private void DisplayMessage() {
-        var dof = ScriptableObject.CreateInstance<DepthOfField>();
-        dof.aperture.Override(aperture);
-        dof.focalLength.Override(focalLength);
-        dof.enabled.Override(true);
-
         ppVolume = blurVolume;
-
+        
         // Blurs scene
         LeanTween.value(gameObject, TweenCallback, 0f, 1f, blurTime);
-
-        // Unblurs scene
-        LeanTween.value(gameObject, TweenCallback, 1f, 0f, blurTime);
 
         foreach (string sentence in sentences) {
             // StopAllCoroutines();
@@ -79,9 +59,8 @@ public class DatalogController : MonoBehaviour
             Debug.Log(sentence);
         }
 
-        void TweenCallback(float newWeight) {
-            blurVolume.weight = newWeight;
-        }
+        // Unblurs scene
+        //LeanTween.value(gameObject, TweenCallback, 1f, 0f, blurTime);
     }
 
     // Pulled from Brackey's tutorial: https://www.youtube.com/watch?v=_nRzoTzeyxU
@@ -93,5 +72,7 @@ public class DatalogController : MonoBehaviour
         }
     }
 
-    
+    void TweenCallback(float newWeight) {
+        blurVolume.weight = newWeight;
+    }
 }
