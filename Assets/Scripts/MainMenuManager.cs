0c98d74253dd596386eb1b9c9c54e46ea831c9ab
Thomas Roberts <robertsorange@gmail.com>
2020-03-25 16:19:08 -0600

    Created main menu

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void StartClicked() {
+        Debug.Log("Loading next scene");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitClicked() {
+        // Note: This will not close the game in the editor, but will once the game is built
+        Application.Quit();
+        // This closes the editor if it is going
+        if (UnityEditor.EditorApplication.isPlaying) {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+        
+    }
+}
