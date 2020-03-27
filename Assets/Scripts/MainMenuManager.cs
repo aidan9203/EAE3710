3142e78651795579fde2d5539af7ebac62e41588
dfmelchert <61022619+dfmelchert@users.noreply.github.com>
2020-03-26 21:15:06 -0600

    update

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -12,9 +12,12 @@ public class MainMenuManager : MonoBehaviour
         // Note: This will not close the game in the editor, but will once the game is built
         Application.Quit();
         // This closes the editor if it is going
+        #if UNITY_EDITOR
+    
         if (UnityEditor.EditorApplication.isPlaying) {
             UnityEditor.EditorApplication.isPlaying = false;
         }
+        #endif
         
     }
 }
