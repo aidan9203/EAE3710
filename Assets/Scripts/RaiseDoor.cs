dbcb2aeaee8bc716b64fc923882412fda0436a92
Thomas Roberts <robertsorange@gmail.com>
2020-04-21 12:34:59 -0600

    Opening door on level 2

---
+++ Assets/Scripts/RaiseDoor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaiseDoor : MonoBehaviour
+{
+    public Transform door;
+
+    void OnTriggerEnter(Collider other) {
+        if(other.CompareTag("Player")) {
+            // Raise door
+            LeanTween.moveLocalY(door.gameObject, 7.25f, 0.5f);
+        }    
+    }
+}
