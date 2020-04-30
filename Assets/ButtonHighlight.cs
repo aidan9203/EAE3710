dc64c9a4c0c2015717838e41ce269dc21e0df073
Thomas Roberts <robertsorange@gmail.com>
2020-04-30 15:24:47 -0600

    Buttons now select on hover

---
+++ Assets/ButtonHighlight.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonHighlight : MonoBehaviour
+{
+    [SerializeField]
+    private EventSystem eventSystem;
+
+    public void MouseHover(BaseEventData data) {
+        var newData = data as PointerEventData;
+        var button = newData.pointerEnter.transform.parent.gameObject;
+
+        eventSystem.SetSelectedGameObject(button);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
